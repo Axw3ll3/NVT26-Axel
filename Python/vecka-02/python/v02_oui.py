d77ef3cf75c7f13c713d7080796f6c0473706c61
Axw3ll3 <axel.hagelbergstridh@gmail.com>
2026-08-24 14:25:42 +0200

    Got done with v02_oui.py exercise, added 3 of my own  MAC addresses and found their vendors and added them as a variable aswell

--- Python/vecka-02/python/v02_oui.py
+++ Python/vecka-02/python/v02_oui.py
@@ -3,12 +3,21 @@ def main():
         "a4:c3:f0": "Intel",
         "3c:d9:2b": "Hewlett-Packard",
         "00:1a:a1": "Cisco Systems",
+        #Own MAC Addresses
+        "a0:ad:9f": "ASUSTek COMPUTER INC",
+        "28:95:29": "Intel Corporate",
+        "E8:78:65": "Apple Inc",
+    
     }
     
     addresses = [
         "a4:c3:f0:11:3a:b7",
         "3c:d9:2b:d2:11:88",
         "8c:85:90:44:12:0e",
+        #Own MAC Addresses
+        "a0:ad:9f:52:03:e3",
+        "28:95:29:c0:50:30",
+        "E8:78:65:04:C5:25",
     ]
     
     for address in addresses:
